b25941e85defc122fbc61111a53aa3aa2e61f420
Nummulith <144725587+Nummulith@users.noreply.github.com>
2024-04-11 13:04:59 +0200

    lab_get

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,8 @@
 ### Readme
+
+Run lab_init.sh (or its content) to start working with labs
+
+Then download labs from the repository and run it:
+
+lab_get helloworld
+helloworld.sh
--- lab_init.sh
+++ lab_init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-get_lab() {
+lab_get() {
     sudo curl -fsSL "https://raw.githubusercontent.com/Nummulith/linux_labs/main/$1.sh" -o "/usr/local/bin/$1.sh"
     sudo chmod +x "/usr/local/bin/$1.sh"
 }
-
-get_lab special_permissions
