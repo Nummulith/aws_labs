7a9a7a0ba637301945903cf1aaa0712fe16c3ef1
Nummulith <144725587+Nummulith@users.noreply.github.com>
2024-04-11 14:12:52 +0200

    sublabs

--- special.sh
+++ special.sh
@@ -39,6 +39,10 @@ cat /lab/show_ids.sh
 
 
 ## Sublabs
+lab_get() {
+    sudo curl -fsSL "https://raw.githubusercontent.com/Nummulith/linux_labs/main/$1.sh" -o "/usr/local/bin/$1.sh"
+    sudo chmod +x "/usr/local/bin/$1.sh"
+}
 
 lab_get special_suid
 lab_get special_sgid
