96a517933d1a505d59da296b7d68e03f9bd3bbdb
Nummulith <144725587+Nummulith@users.noreply.github.com>
2024-04-13 01:27:51 +0200

    Update special.sh

--- special.sh
+++ special.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Header
+echo "
+////////////////////////
+//
+//  Special permissions
+//
+"
 
-## Users and group
-
+# Users and group
 sudo useradd foo
 sudo useradd bar
 sudo groupadd labusers
@@ -14,9 +20,7 @@ echo "Users and group are created:"
 cut -d: -f1,3 /etc/group | awk -F: '$2 >= 1000 && $2 < 65534 {print $1":"$2}'
 echo
 
-
-## Lab directory
-
+# Lab directory
 sudo mkdir /lab
 sudo chown ec2-user:labusers /lab
 
@@ -26,29 +30,22 @@ echo "lab folder created:"
 ls -ld /lab
 echo
 
-
 # bash show ids script (does not work)
-
 #echo '#!/bin/bash
 #echo "uid: $(id -un), gid: $(id -gn)"' > /lab/show_ids.sh
 
-
-## gcc
-
+# gcc
 sudo yum install -yq gcc
 
 echo 'gcc installed:'
 yum info gcc | grep Version
 echo
 
-
-## show_ids script
-
+# show_ids script
 echo '#include <stdio.h>
 #include <unistd.h>
 #include <pwd.h>
 #include <grp.h>
-
 int main() {
     uid_t ruid = getuid(); gid_t rgid = getgid(); uid_t euid = geteuid(); gid_t egid = getegid();
     printf("Real      user: %s(%d):%s(%d)\n", getpwuid(ruid)->pw_name, ruid, getgrgid(rgid)->gr_name, rgid);
@@ -60,20 +57,16 @@ echo "Show ids script:"
 cat show_ids.c
 echo
 
-
-## Compiling script
-
+# Compiling script
 gcc show_ids.c -o show_ids
 
 sudo chown foo:bar show_ids
-chmod +x show_ids
 
 echo "Show ids script:"
 ls -l show_ids
 echo
 
-
-## Sublabs
+# Sublabs
 lab_get() {
     sudo curl -fsSL "https://raw.githubusercontent.com/Nummulith/linux_labs/main/$1.sh" -o "/usr/local/bin/$1.sh"
     sudo chmod +x "/usr/local/bin/$1.sh"
@@ -87,6 +80,10 @@ echo "Sublabs downloaded:"
 ls /usr/local/bin/special_*.sh
 echo
 
-
 # Next
-echo "Run special_suid.sh"
+echo "
+//
+//  Next run special_suid.sh
+//
+////////////////////////
+"
