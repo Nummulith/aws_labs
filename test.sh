28e7c1d74774d9fe338399e76fc04a65ae112eda
Nummulith <144725587+Nummulith@users.noreply.github.com>
2024-04-15 14:32:32 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -v
 
-## Test
-
 sudo groupadd lab_users
 sudo usermod -aG lab_users ec2-user
 
-sudo mkdir /tst
-sudo chown ec2-user:lab_users /tst
-sudo chmod g+w /tst
+sudo mkdir dir
+sudo chown ec2-user:lab_users dir
 
-ls -ld /tst
+touch file
 
-chmod u-s,g+s /tst
+ls -ld dir
 
-ls -ld /tst
+chmod u-s,g+s dir
+chmod u-s,g+s file
+
+ls -ld dir
