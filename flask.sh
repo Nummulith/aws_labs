e69b8f6bd5db3a43addc6fde3dd07b72f30474dd
pavel.eresko@dci <pavel.eresko@dci-student.org>
2024-04-22 12:38:08 +0200

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -1,17 +1,16 @@
 sudo yum update -y
 python3 --version
-pip3 --version
 python3 -m ensurepip
 pip3 --version
 pip3 install --upgrade pip
 pip3 install flask
 flask --version
-pwd
-ls
-sudo nano app.py
-from flask import Flask
+sudo echo 'from flask import Flask
 app = Flask(__name__)
 @app.get('/')
 def index():
   return 'Hello world'
+' > app.py
+pwd
+ls
 flask run --debug --host=0.0.0.0
