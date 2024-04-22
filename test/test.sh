d309db0667f5eac2c00072299e2c7b1f53714264
pavel.eresko@dci <pavel.eresko@dci-student.org>
2024-04-22 19:15:16 +0200

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -5,8 +5,8 @@
 set -x
 
 file_get() {
-    sudo curl -fsSL "https://raw.githubusercontent.com/Nummulith/linux_labs/main/test/$1" -o "~/$1"
-    sudo chmod +x "~/$1"
+    sudo curl -fsSL "https://raw.githubusercontent.com/Nummulith/linux_labs/main/test$1" -o "~$1"
+    sudo chmod +x "~$1"
 }
 
 file_get /test/test.txt
