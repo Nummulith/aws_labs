2cdc3652e21f0b1c930a6d9fef438f2c61bad953
pavel.eresko@dci <pavel.eresko@dci-student.org>
2024-04-22 19:42:28 +0200

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -3,8 +3,8 @@
 ## TEST
 
 file_get() {
-    curl -fsSL "https://raw.githubusercontent.com/Nummulith/linux_labs/main/test$1" -o "~$1"
-    chmod +x "~$1"
+    curl -fsSL "https://raw.githubusercontent.com/Nummulith/linux_labs/main/test/$1" -o ~/$1;
+    chmod +x ~/$1;
 }
 
 mkdir test
